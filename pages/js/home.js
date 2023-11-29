d816d68264f712ba7a2dc90f8bed7571053787ee
Tuvdennamjil <144209440+Tuvdennamjil@users.noreply.github.com>
2023-11-29 18:07:23 +0800

    update

--- pages/js/home.js
+++ pages/js/home.js
@@ -22,54 +22,52 @@ function plusSlides(n) {
 
 // Sample data for promotion products
 const promotionData = [
-  {  name: 'Choco melon', description: 'Double choco melon pan 100гр',  Price: '2750₮', image: 'product5.png',discount: '50%',discountingPrice: '5500₮',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne' },
-  {  name: 'Coffee cream pan', description: 'Coffee cream pan 100гр', Price: '5500₮', image: 'product6.png',discount: '1+1',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne'  },
-  { name: 'Мөстэй американо', description: 'CU 120гр', Price: '3150₮', image: 'Product7.png', discount: '30%', discountingPrice: '4500₮',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne'  },
-  { name: 'Алаг донат', description: 'CU 80гр', discountedPrice: '3500₮', image: 'product8.png',discount: '1+1',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne'  },
+  {  name: 'Chocoo melon', description: 'Double choco melon pan 100гр',  Price: '2750₮', image: 'product5.png',discount: '50%',discountingPrice: '5500₮',manufacturer:'apu',weight:'100oгр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'0' },
+  {  name: 'Coffee cream pan', description: 'Coffee cream pan 100гр', Price: '55000₮', image: 'product6.png',discount: '1+1',discountingPrice:'',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'1'},
+  { name: 'Мөстэй американо', description: 'CU 120гр', Price: '3150₮', image: 'Product7.png', discount: '30%', discountingPrice: '4500₮',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'2'},
+  { name: 'Алаг донат', description: 'CU 80гр',Price:'3150',image: 'product8.png',discount: '1+1',discountedPrice: '3500₮',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'3'}
 ];
 const productData = [
-  {  name: 'Choco melon', description: 'Double choco melon pan 100гр',  Price: '2750₮', image: 'product5.png',discount: '50%',discountingPrice: '5500₮' ,manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne' },
-  {  name: 'Coffee cream pan', description: 'Coffee cream pan 100гр', Price: '5500₮', image: 'product6.png',discount: '1+1',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne'  },
-  { name: 'Мөстэй американо', description: 'CU 120гр', Price: '3150₮', image: 'Product7.png', discount: '30%', discountingPrice: '4500₮' ,manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne' },
-  { name: 'Алаг донат', description: 'CU 80гр', discountedPrice: '3500₮', image: 'product8.png',discount: '1+1',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne'  },
-  {  name: 'Choco melon', description: 'Double choco melon pan 100гр',  Price: '2750₮', image: 'product5.png',discount: '50%',discountingPrice: '5500₮',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne'  },
-  {  name: 'Coffee cream pan', description: 'Coffee cream pan 100гр', Price: '5500₮', image: 'product6.png',discount: '1+1' ,manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne' },
-  { name: 'Мөстэй американо', description: 'CU 120гр', Price: '3150₮', image: 'Product7.png', discount: '30%', discountingPrice: '4500₮',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne'  },
-  { name: 'Алаг донат', description: 'CU 80гр', discountedPrice: '3500₮', image: 'product8.png',discount: '1+1',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne'  },
+  {  name: 'Choco melon', description: 'Double choco melon pan 100гр',  Price: '2750₮', image: 'product5.png',discount: '50%',discountingPrice: '5500₮' ,manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'4'},
+  {  name: 'Coffee cream pan', description: 'Coffee cream pan 100гр', Price: '5500₮', image: 'product6.png',discount: '1+1',discountingPrice:'',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'5'},
+  { name: 'Мөстэй американо', description: 'CU 120гр', Price: '3150₮', image: 'Product7.png', discount: '30%', discountingPrice: '4500₮' ,manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'6' },
+  { name: 'Алаг донат', description: 'CU 80гр', discountedPrice: '3500₮', image: 'product8.png',discount: '1+1',discountingPRice:'',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'7'  },
+  {  name: 'Choco melon', description: 'Double choco melon pan 100гр',  Price: '2750₮', image: 'product5.png',discount: '50%',discountingPrice: '5500₮',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'8'  },
+  {  name: 'Coffee cream pan', description: 'Coffee cream pan 100гр', Price: '5500₮', image: 'product6.png',discount: '1+1' ,discountingPrice:'',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'9' },
+  { name: 'Мөстэй американо', description: 'CU 120гр', Price: '3150₮', image: 'Product7.png', discount: '30%', discountingPrice: '4500₮',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne',More:'10' },
+  { name: 'Алаг донат', description: 'CU 80гр', discountedPrice: '3500₮', image: 'product8.png',discount: '1+1',discountingPrice:'',manufacturer:'apu',weight:'100гр',expiration:'unuudur',ingredients:'uzem talh',instructions:'unuuudur',storage:'udurtu herglne' ,More:'11' }
 ];
-
 class Product {
-    constructor(name, description, Price, image, discount, discountingPrice,manufacturer,weight,expiration,ingredients,instructions,storage) {
-      this.discount = discount||null;
-      this.name = name;
-      this.description = description;
-      this.discount = discount;
-      this.Price = Price ||null;
-      this.discountingPrice = discountingPrice;
-      this.image = image;
-      this.manufacturer=manufacturer;
-      this.weight=weight;
-      this.expiration=expiration;
-      this.ingredients=ingredients;
-      this.instructions=instructions;
-      this.storage=storage;
-    }
-    
+  constructor(name, description, Price, image, discount, discountingPrice, manufacturer, weight, expiration, ingredients, instructions, storage, More) {
+    this.discount = discount || null;
+    this.name = name;
+    this.description = description;
+    this.discount = discount;
+    this.Price = Price || null;
+    this.discountingPrice = discountingPrice;
+    this.image = image;
+    this.manufacturer = manufacturer;
+    this.weight = weight;
+    this.expiration = expiration;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+    this.storage = storage;
+    this.More = More;
+  }
 
-
-    generateHTML() {
-        let html = `
-          <article class="product">
+  generateHTML() {
+    let html = `
+          <article class="product" >
           `;
             
             if (this.discount != null) {
                 html += `<span>${this.discount}</span>`;
               }
             html += `
-            <a href="Product.html">
+            <div id='${this.More}'>
               <img src="assets/png/${this.image}" alt="${this.name}">
               <h3>${this.name}</h3>
-            </a>
+            </div>
             <p>${this.description}</p>
             
             `;
@@ -80,64 +78,93 @@ class Product {
       
         html += `
             <strong>${this.Price}</strong>
-            <button>Сагслах</button>
+            <button class="popup-button" id="${this.More}">Үзэх</button>
           </article>
-          <div class="productGet">
-          <div class="productExit">
-              <span id="btnClose" class>&times;</span>
-          </div>
-          <article class="productDescription">
-              <h2>${this.name}</h2>
-              <div class="productPrice">
-                  <h2>${this.Price}</h2>
-                  <h2>${this.Price}</h2>
-              </div>
-              <div class="productDetails">
-                  <p>${this.manufacturer}:</p>
-                  <p>${this.weight}:</p>
-                  <p>${this.expiration}:</p>
-                  <p>${this.ingredients}:</p>
-                  <p>${this.instructions}:</p>
-                  <p>${this.storage}:</p>
-              </div>
-              <div class="buttons">
-                  <button>авах</button>
-                  <button>❤️</button>
-                  <button>Сагслах</button>
-                  <div class="productAdd">
-                      <button>+</button>
-                      <button>0</button>
-                      <button>-</button>
-                  </div>
-              </div>
-          </article>
-          <figure><img src="assets/png/product1.png" alt=""></figure>
-      </div>`
+        `
         ;
-      
-        return html;
+        html +=`
+        <div class="productGet" data-id="${this.More}">
+        <div class="productExit">
+            <span class="close-popup">&times;</span>
+        </div>
+        <article class="productDescription" id=>
+            <h2>${this.name}</h2>
+            <div class="productPrice">
+                <h2>${this.Price}</h2>
+                <h2>${this.Price}</h2>
+            </div>
+            <div class="productDetails">
+                <p>Үйлдвэрлэгч:${this.manufacturer}</p>
+                <p>Жин:${this.weight}</p>
+                <p>Дуусах хугацаа:${this.expiration}</p>
+                <p>Орц:${this.ingredients}</p>
+                <p>Хэргэлэх Заавар${this.instructions}</p>
+                <p>Хадгалах горим:${this.storage}</p>
+            </div>
+            <div class="buttons">
+                <button>авах</button>
+                <button>❤️</button>
+                <button>Сагслах</button>
+                <div class="productAdd">
+                    <button>+</button>
+                    <button>0</button>
+                    <button>-</button>
+                </div>
+            </div>
+        </article>
+        <figure><img src="assets/png/${this.image}" alt=""></figure>
+    </div>`
+        return html
+
+  
+    
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const promotionProductsContainer = document.getElementById('promotionProducts');
+    promotionData.forEach(productInfo => {
+    const product = new Product(...Object.values(productInfo));
+    promotionProductsContainer.insertAdjacentHTML('beforeend', product.generateHTML());
+  });
+  const normalProductsContainer = document.getElementById('normalProducts');
+  productData.forEach(productInfo => {
+    const product = new Product(...Object.values(productInfo));
+    normalProductsContainer.insertAdjacentHTML('beforeend', product.generateHTML());
+  });
+  document.addEventListener('click',function(event){
+      if(event.target.classList.contains('popup-button')){
+        const productId=event.target.id;
+        openPopup(productId);
       }
+  });
+  document.addEventListener('click',function(event){
+      if(event.target.classList.contains('close-popup')){        
+        ClosePopup();
+      }
+  })
+});
+  function openPopup(productId){
+    const popup=document.querySelector(`.productGet[data-id="${productId}"]`);
+    if(popup){
+      popup.classList.add('active');
+      backdrop.classList.add('show');
+    }
+  }
+  function ClosePopup(){
+    const activePopup=document.querySelector(`.productGet.active`);
+    if(activePopup){
+      activePopup.classList.remove('active');
+      backdrop.classList.remove('show');
+    }
   }
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const promotionProductsContainer = document.getElementById('promotionProducts');
-      promotionData.forEach(productInfo => {
-      const product = new Product(...Object.values(productInfo));
-      promotionProductsContainer.insertAdjacentHTML('beforeend', product.generateHTML());
-    });
-  });
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const promotionProductsContainer = document.getElementById('normalProducts');
-    productData.forEach(productInfo => {
-      const product = new Product(...Object.values(productInfo));
-      promotionProductsContainer.insertAdjacentHTML('beforeend', product.generateHTML());
-    });
-  });
+  
 
 
-    document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", function () {
         const carousel = document.querySelector(".carousel");
         let currentIndex = 0;
 
@@ -164,7 +191,7 @@ class Product {
         document.querySelector(".pre").addEventListener("click", prevImage);
     });
 
-
+// sagsni neegdej haagdah code
 const openBtn=document.getElementById('openBasketBtn');
 const basket=document.getElementById('sideBasket');
 const closeBtn=document.getElementById('btnClose');
@@ -182,3 +209,5 @@ function closeBasket(){
     basket.classList.remove('open');
     backdrop.classList.remove('show');
 }
+
+
